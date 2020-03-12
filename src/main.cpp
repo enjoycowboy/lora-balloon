29abc3648748d819e0be97e336c1a0a976b92e7e
enjoycowboyu <eveiga@msn.com>
2020-03-12 16:55:15 +0000

    exemplo do lora

--- src/main.cpp
+++ src/main.cpp
@@ -34,9 +34,9 @@ uint8_t fifoBuffer[64]; // FIFO storage buffer
 Quaternion q;		 // [w, x, y, z]         quaternion container
 VectorInt16 aa;		 // [x, y, z]            accel sensor measurements
 VectorInt16 gy;		 // [x, y, z]            gyro sensor measurements
-VectorInt16 aaReal;  // [x, y, z]            gravity-free accel sensor measurements
-VectorInt16 aaWorld; // [x, y, z]            world-frame accel sensor measurements
-VectorFloat gravity; // [x, y, z]            gravity vector
+VectorInt16 aaReal; 	 // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;	 // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;	 // [x, y, z]            gravity vector
 float euler[3];		 // [psi, theta, phi]    Euler angle container
 float ypr[3];		 // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
 
